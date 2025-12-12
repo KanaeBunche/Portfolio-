ca2f5689954e2e24222e2b40b266499f892121f6
Kanae <kanae.bunche@gmail.com>
2025-12-12 12:59:20 -0500

    Add Vercel Analytics (Vite)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { inject } from "@vercel/analytics";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+inject(); // 👈 THIS enables analytics
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
